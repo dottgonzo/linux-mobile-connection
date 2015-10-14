396c6e5508f7d936afc5b7cad9fd62ab5d02cc18
Dario Caruso <darioyzf@gmail.com>
2015-10-15 01:35:05 +0200

    start

--- index.js
+++ index.js
@@ -1,91 +1,136 @@
-var mobilemodem=require('wvdialjs'),
+var Wvdial=require('wvdialjs'),
 pathExists=require('path-exists'),
 merge=require('json-add'),
+Promise=require('promise')
 lsusbdev=require('lsusbdev');
 
 function goconnect(provider,options){
+  return new Promise(function (resolve, reject) {
 
-  if(options.dev){
+    var mobilemodem=new Wvdial(options.wvdialFile);
 
-    if(pathExists.sync('/sys/bus/usb/devices/'+options.dev)){
+    if(options.dev){
 
-      lsusbdev().then(function(data){
-        for(var i=0;i<data.length;i++){
-          var usb=data[i];
+      if(pathExists.sync('/sys/bus/usb/devices/'+options.dev)){
 
-          if(usb.type=='serial'&&usb.hub==options.dev){
-            if(pathExists.sync('/etc/wvdial.conf')){
-              mobilemodem.setUsb(usb.dev).then(function(){
-                mobilemodem.connect()
+        lsusbdev().then(function(data){
 
-              })
+          for(var i=0;i<data.length;i++){
+            var usb=data[i];
 
+            if(usb.type=='serial'&&usb.hub==options.dev){
+              if(pathExists.sync(options.wvdialFile)){
+                mobilemodem.setUsb(usb.dev).then(function(){
+                  mobilemodem.connect().then(function(){
+                    resolve(success:true);
+                  }).catch(function(err){
+                    reject(err)
+                  })
+                }).catch(function(err){
+                  reject(err)
+                })
+
+              } else{
+                mobilemodem.configure(provider).then(function(){
+                  mobilemodem.setUsb(usb.dev).then(function(){
+                    mobilemodem.connect().then(function(){
+                      resolve(success:true);
+                    }).catch(function(err){
+                      reject(err)
+                    })
+                  }).catch(function(err){
+                    reject(err)
+                  })
+                }).catch(function(err){
+                  reject(err)
+                })
+              }
             }
-
           }
-        }
+        })
+      } else{
+        reject({error:"Wrong device"})
 
+      }
+    } else {
+      mobilemodem.configure(provider).then(function(){
+        mobilemodem.connect().then(function(){
+          resolve(success:true);
+        }).catch(function(err){
+          reject(err)
+        })
+      }).catch(function(err){
+        reject(err)
       })
+
     }
-  } else {
-    mobilemodem.configure(config.devices.mobilemodem.provider).then(function(){
-      mobilemodem.connect()
-
-    })
-
-  }
-
+  })
 }
 
 module.exports=function(provider,opt){
+  return new Promise(function (resolve, reject) {
 
-  var options={};
-  options.timer=false;
-  options.verbose=true
+    var options={};
+    options.timer=false;
+    options.verbose=true;
+    options.wvdialFile='/etc/wvdial.conf';
+    options.dev=false;
+    options.ifOffline=true;
+    options.reconnect=false;
+    options.retry=false;
+    options.retryMax=false;
 
-  options.dev=false
-  options.ifOffline=true
-  options.reconnect=false
-  options.retry=false
-  options.retryMax=false
+    merge(options,opt);
 
-  merge(options,opt);
-  if (provider && provider.apn){
+    if (provider && provider.apn){
 
-  if(options.timer){
-    setTimeout(function () {
+      if(options.timer){
 
-      if(options.ifOffline){
-        testConnection().catch(function(err){
+        setTimeout(function () {
 
+          if(options.ifOffline){
 
-          goconnect(provider,options);
+            testConnection().then(function(){
+              reject({online:true});
+            }).catch(function(err){
+              goconnect(provider,options).catch(function(err){
+                reject(err)
+              })
+            })
 
+          } else{
 
+            goconnect(provider,options).catch(function(err){
+              reject(err)
+            })
 
-        })
+          }
+        }, options.timer);
       } else{
-        goconnect(provider,options);
+
+        if(options.ifOffline){
+
+          testConnection().then(function(){
+            reject({online:true});
+          }).catch(function(err){
+            goconnect(provider,options).catch(function(err){
+              reject(err)
+            })
+          })
+
+        } else{
+
+          goconnect(provider,options).catch(function(err){
+            reject(err)
+          })
+
+        }
 
       }
-    }, options.timer);
-  } else{
-    if(options.ifOffline){
-      testConnection().catch(function(err){
 
-
-        goconnect(provider,options);
-
-
-
-      })
     } else{
-      goconnect(provider,options);
-
+      reject({error:"You must provide a valid Apn"})
     }
 
-
-  }
-
-}
+  })
 }

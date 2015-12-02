89ce62f66ec2268d4fdb02e626f4e2f7761e4892
Dario Caruso <darioyzf@gmail.com>
2015-12-02 21:28:35 +0100

    oki

--- run/tre.js
+++ run/tre.js
@@ -4,17 +4,17 @@ verb=require('verbo');
 
 mobileconnect({"label":"Tre Ricaricabile","apn":"tre.it","phone":"*99#","username":"tre","password":"tre"},{retry:false}).then(function(answer){
 
-  setTimeout(function () {
-    netw.data().then(function(doc){
-      if (doc.network){
-      verb("connected to "+doc.network.dev+' with '+doc.network.ip,"info","Online")
-    } else{
-      verb("Offline","warn","Offline")
-
-    }
-
-    })
-  }, 100000);
+  // setTimeout(function () {
+  //   netw.data().then(function(doc){
+  //     if (doc.network){
+  //     verb("connected to "+doc.network.dev+' with '+doc.network.ip,"info","Online")
+  //   } else{
+  //     verb("Offline","warn","Offline")
+  //
+  //   }
+  //
+  //   })
+  // }, 100000);
 
 
 
